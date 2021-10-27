65adc035283f6ccb7ed06aa4cdafeadc0c79fd76
maria-grig <masha.grigorenko.89@gmail.com>
2021-10-27 22:13:08 +0300

    final commit

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -31,4 +31,29 @@ class BonusServiceTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void shouldCalculateNotRegisteredBonusUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_00;
+        boolean registered = false;
+        long expected = 10;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateNotRegisteredBonusOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 100_000_00;
+        boolean registered = false;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
 }
