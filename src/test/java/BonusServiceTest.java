436177d2d543d83a877d206ad2db5a6d00cd3b33
maria-grig <masha.grigorenko.89@gmail.com>
2022-01-25 18:36:59 +0300

    CsvFileSource fixed

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,15 +1,17 @@
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import ru.netology.bonus.BonusService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BonusServiceTest {
+
     BonusService service = new BonusService();
 
     @ParameterizedTest
-    @CsvFileSource(resources = {"/data/data.csv"}, delimiter = '|', numLinesToSkip = 2);
+    @CsvFileSource(resources = "/data/data.csv", delimiter = '|', numLinesToSkip = 2)
 
-    void calculateWithCsvFileSource(String test, long amount, boolean registered, long expected) {
+        void calculateWithCsvFileSource(String test, long amount, boolean registered, long expected) {
         long actual = service.calculate(amount, registered);
         assertEquals(expected, actual);
     }
